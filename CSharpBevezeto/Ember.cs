9819cece8c71f3d2df0ced57787a233a33881c48
aprobence <aprobence2017@gmail.com>
2025-02-04 09:01:44 +0100

    Kész program

--- CSharpBevezeto/Ember.cs
+++ CSharpBevezeto/Ember.cs
@@ -2,5 +2,36 @@ namespace CSharpBevezeto;
 
 public class Ember
 {
-    
+    private double testSuly { get; set; }
+    private int testMagassag { get; set; }
+
+    public void Peldany(double suly, int magassag)
+    {
+        testSuly = suly;
+        testMagassag = magassag;
+    }
+
+    public override string ToString()
+    {
+        return $"Test Súly: {testSuly}, Magasság: {testMagassag}";
+    }
+
+    public double TestTomegIndex(double suly, int magassag)
+    {
+        double mgs = magassag / 100.0;
+        double tti = suly / (mgs * mgs);
+        return Math.Round(tti, 2);
+    }
+
+    public bool NormalTTI(double tti)
+    {
+        if (tti >= 18.5 && tti < 25)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
--- CSharpBevezeto/Program.cs
+++ CSharpBevezeto/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Ember ember = new Ember();
+        
+        Random rnd = new Random();
+        int magassag = rnd.Next(150, 190);
+        double suly = rnd.Next(40, 100);
+        ember.Peldany(suly, magassag);
+
+        Console.WriteLine(ember.ToString());
+        double tti = ember.TestTomegIndex(suly, magassag);
+        Console.WriteLine(tti);
+        Console.WriteLine(ember.NormalTTI(tti));
     }
 }
